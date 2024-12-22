ae2143ac881dd9b056e2670987828621ed531453
riskiauriza <xernonnen@gmail.com>
2024-12-22 22:13:54 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,16 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4917895&101"/>');
+
+///// Statcounter /////
+var sc_project=13071655; 
+var sc_invisible=1; 
+var sc_security="6edc3158";
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13071655/0/6edc3158/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
