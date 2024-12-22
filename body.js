34ca3ae7e00c746d981c4ee2719044d541207335
riskiauriza <xernonnen@gmail.com>
2024-12-22 22:15:06 +0700

    Create body.js

---
+++ body.js
@@ -0,0 +1,11 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+document.write('<script type="text/javascript" src="https://riskiauriza.github.io/lampuhijaukita/ads.js"><\/script>');
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
