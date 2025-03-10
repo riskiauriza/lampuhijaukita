c77076057e2e9efc271026329dd644128583e4b5
riskiauriza <xernonnen@gmail.com>
2025-03-10 08:54:48 -0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,9 +1,9 @@
 
  atOptions = {
-		'key' : '2ee5a4b4c22d73b53a2f4bd04c3ef50e',
+		'key' : '274ddc30d4e73bae19cf1299a0efaac0',
 		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
+		'height' : 60,
+		'width' : 468,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/2ee5a4b4c22d73b53a2f4bd04c3ef50e/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/274ddc30d4e73bae19cf1299a0efaac0/invoke.js"></scr' + 'ipt>');
