821f22e61b7772ad69061935e885b9df45a65f4a
riskiauriza <xernonnen@gmail.com>
2025-04-11 21:30:22 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -7,3 +7,6 @@
 		'params' : {}
 	};
 document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/25f1c0a89a1f23aaab114ffdbdf24975/invoke.js"></scr' + 'ipt>');
+
+document.write('<scr' + 'ipt type="text/javascript" src="//pl26346364.profitableratecpm.com/1b/3a/68/1b3a68d7519c80b1e156549e4f84cb4a.js"></scr' + 'ipt>');
+
